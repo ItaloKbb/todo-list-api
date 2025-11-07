6cee28e2c0de33445a74ac1001409307ab9a0c92
The-Loop-Code <theloopcode@gmail.com>
2025-11-07 12:11:43 -0300

    Definição de host para deploy

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import app from './app.js';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3333;
+const HOST = process.env.HOST || '0.0.0.0';
 
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`🚀 Servidor rodando em http://${HOST}:${PORT}`);
 });
